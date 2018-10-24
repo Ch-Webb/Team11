ce81d04da79ef932742974c7cc1d4ce3044b8ac3
Charlie Webb <cjsw00@hotmail.co.uk>
2018-10-24 21:41:53 +0100

    Added a few words to skip list

--- group_game1.py
+++ group_game1.py
@@ -3,7 +3,7 @@ import random
 from items import *
 from areas import *
 skip_words = ['a', 'about', 'all', 'an', 'another', 'any', 'around', 'at',
-              'bad', 'beautiful', 'been', 'better', 'big', 'can', 'every', 'for',
+              'bad', 'beautiful', 'been', 'better', 'big', 'can', 'every', 'fat', 'for',
               'from', 'good', 'have', 'her', 'here', 'hers', 'his', 'how',
               'i', 'if', 'in', 'into', 'is', 'it', 'its', 'large', 'later',
               'like', 'little', 'main', 'me', 'mine', 'more', 'my', 'now',
