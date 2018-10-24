1f8fb884fd18d8f306b02091c0100838a393ecf2
kyleswirethompson <kyle.swirethompson@gmail.com>
2018-10-24 13:01:16 +0100

    adding in speech between people in game

--- group_game1.py
+++ group_game1.py
@@ -219,44 +219,225 @@ def execute_look(character):
 def execute_buy():
     global inventory
     if item_wallet and item_id in inventory:
-        print("BUYING drink SCRIPT")
-        input("------->PRESS ENTER TO CARRY ON<------")
-        inventory.append(item_drink)
+        if talk_too_girl == True:
+
+            print("""
+You wander over to the bar and lean on it, the room spinning around you. The bartender wanders back over with an arm full of empty VKs and tips them into the bin.
+
+He turns to you and raises an eyebrow.
+
+“Well?”
+
+“She wants a drink before she’ll give you her number mate.”
+
+“smirnoff vodka, then. She’s been on them all night.”
+
+“Alright, sounds good.”
+
+He swiftly pours out a measure of Smirnoff and tops it up with Coke from the handheld tap behind the bar.
+
+“This Smirnoff is nothing like real Russian vodka, like I used to have in Moscow. It’s like water in comparison.”
+
+You nod, grabbing the drink as he sets it down on the bar.
+
+“I’ll be back with her number in a minute,” you say, wandering away from the bar.
+            """)
+            input("------->PRESS ENTER TO CARRY ON<------")
+            inventory.append(item_drink)
+        else:
+
+            print("""
+calling the bartender over
+
+"one vodka please"
+
+"got your wallet and id?"
+
+"yes" you say annoyed as you pull them out to show him
+
+he pours your drink and slides it too you
+
+you pay and looking at it down it in one....... you idiot
+
+
+""")
+            input("------->PRESS ENTER TO CARRY ON<------")
     else:
-        print("GETTING REFUSED SCRIPT")
+        print("""I TOLD YOU, NO MONEY AND NO ID AND YOUR NOT GETTING A DRINK, the bartender says to you looking very annoyed""")
         input("------->PRESS ENTER TO CARRY ON<------")
         
 def execute_talk(person):
     global inventory
     if person == "barman" and current_area == bar_area:
         if item_number in inventory: 
-            print("GIVE BARMAN NUMBER SCRIPT")
+            print("""
+You wander back to the bar yet again and lean on it, calling the bartender back over. He practically sprints back over, hope in his eyes.
+
+“Well, did you get her number?”
+
+You hold out your hand with the number scribbled on it. The bartender holds onto it for dear life, looking deeply into your eyes
+
+“Um. No. Mate. The number’s on my hand.”
+
+He blushes bright red and lets go as if he’s had an electric shock.
+
+“Oh! Right. Sorry. Er… right. I’ll copy that down. What do you even do on a first date? Maybe we could listen to Tchaikovsky… no, no. Maybe just play Monopoly?”
+
+He takes out his phone and keys in the number, saving it into his contacts, rambling on about Moscovian Monopoly and computer generated Tchaikovsky overtures. He then looks back up at you and nods towards the door.
+
+“One of the bouncers picked up your phone. He’s outside now, a gentleman called Kirill. Average height, glasses, slight bit of a beard, dark hair. Russian bloke.”
+
+“Like you, then?” You ask.
+
+“Yeah, people say we look alike. I can’t see it myself.”
+
+“Thanks.”
+
+You turn from the bar and wander away. Time to get your phone off Kirill the Bouncer and go home, be sick into your toilet and pass out on the bedroom floor with your feet up the wall.
+""")
             input("------->PRESS ENTER TO GIVE BARMAN THE GIRL'S NUMBER<------")
             inventory.remove(item_number)           
         else:         
-            print("PRINT BARMAN SCRIPT")
+            print("""You approach the bartender and look him up and down. You notice a small name tag on his black shirt that reads “K Sidorov.”
+
+He wanders up and down the bar collecting empty VK bottles, then returns to you.
+
+He peers over his glasses at you and scratches at his facial hair.
+
+“We’re actually about to close,” he says, with a slight Russian accent.
+
+“I need to find my phone, I lost it on the dance floor I think.”
+
+“Oh, that was you. I see. Well…”
+
+He looks at his watch, then peers back to you.
+
+“Tell you what, you help me and I’ll help you?” he says, raising an eyebrow.
+
+“Oh ho, a quid pro quo?”
+
+“I suppose.”
+
+He flicks his glance towards the table area, pointing towards a certain individual.
+
+“See that girl at the table? I’ve been trying to get her number all night but I can’t get out from behind this damn bar. You get me her number, I’ll see if I can find out where your phone is. Fair?”
+
+You sigh, slightly, then nod your head. You couldn’t help thinking that Kirill looked a bit like your mate.
+
+Then again, people say that you and your mate look alike, but you’ve never seen it. Time to go get the bartender that girls number, you suppose. """)
+            talk_too_girl = True
             input("------->PRESS ENTER TO CARRY ON<------")
         return
     elif person == "girl" and current_area ==  table_area :
+        talk_too_girl = True
         if item_drink in inventory:
-            print("print give girl drink")
+            print("""
+You approach the girl on the dancefloor again and she turns to face you with a glint him her bespectacled eyes.
+
+She asks in a vaguely Russian accent, “Is that for me?”
+
+She eyes the vodka and coke hungrily. You nod, passing it over. She grabs it and skulls the whole thing, necking it in the way only a half-drunk fresher can.
+
+“Alright, I did promise you my number, didn’t I?”
+
+You take out a pen and scribble the number down on your hand as best you can, with the VKs and Jägerbombs coursing through your veins.
+
+You nod your thanks and take your leave as the bearded girl continues to dance. Maybe now the bartender will tell you where your phone is.
+
+
+""")
             input("------->PRESS ENTER TO GIVE GIRL DRINK<------")
             inventory.remove(item_drink)
             inventory.append(item_number)
-        else:            
-            print("PRINT GET GIRL NUMBERS SCRIPT")
+        else:
+            talk_too_girl = True
+            print("""You approach the girl the bartender pointed out and attempt to strike up a conversation. She eventually turns to you and peers at you through her glasses.
+
+“Oh hello! Having a nice night?”
+
+She asks, with an accent not unlike the bartender’s. Now you think about it, she’s got a beard a bit like the bartender’s as well. Whatever he’s into, you guess.
+
+“Yeah, thanks! Look, the bloke at the bar wants your number but he can’t get round here to ask because he’s on shift. Any chance I could take it for him?”
+
+She replies in her quietly accented voice, “Well, aren’t you sweet. Tell you what, if you can get me a drink, I’ll give you my number for that cute bartender. Thanks!”
+
+And with that, she turned back towards the speakers near the tables and carried on dancing.
+
+You roll your eyes and glance toward the bartender. You just want to go home and pass out already.""")
             input("------->PRESS ENTER TO CARRY ON<------")
         return
     elif person == "mate" and current_area == seating_area :
-        print("PRINT TALK TO MATE SCRIPT")
-        print("PRINT MATE ASKED FOR ID")
+        print("""
+You approach your friend on the dance floor, the pounding techno making your teeth rattle in your head.
+
+He turns to face you, revealing dark hair drenched in sweat from the heat of the dance floor and slightly hazy eyes under his square glasses, probably from the VKs.
+
+He shouts in slightly accented English over the blaring music.
+
+“There you are! I have been looking everywhere for you!
+
+"I'VE LOST MY ID" he says to you, "can you help me find it?"
+""")
+        input("------->PRESS ENTER TO CARRY ON<------")
+
         if item_mates_id in inventory:
-            input("------->PRESS ENTER TO GIVE HIM HIS ID<------")        
+            input("------->PRESS ENTER TO GIVE HIM HIS ID<------")
+            print("As he reaches to put away his id he relises that he already has an id, your id, as well as your wallet.")
+            print("""You look wrecked mate, you should probably get going home. You dropped your wallet and your keys earlier and then wandered off, so I grabbed them for you. Here you go!”
+
+He hands over the keys and the wallet. Opening your wallet, you see your ID, confirming you are indeed over 18.
+
+You thank him quietly, trying to remember what else you needed before you could leave the club.
+""")
             inventory.append(item_wallet)
             inventory.append(item_id)
             inventory.remove(item_mates_id)
         input("------->PRESS ENTER TO CARRY ON<------")
         return
+
+    elif person == "bouncer" and current_area == exit_area :
+
+        print("""
+You approach one of the bouncers, an average height, dark haired bloke. He turns to face you, revealing square glasses and a light dusting of hair on his chin.
+
+He speaks softly, in a distinctly Russian accent. A name tag on his chest bears the name “Dr Kirill Sidorov.” What the hell is a bouncer doing with a doctorate?
+
+“Can I help you, my friend?”
+
+The man looks at you as if he’s had just about enough of his job, so you decide to make it quick.
+
+“Hi, the bartender said you picked up my phone when I dropped it earlier?”
+
+He narrows his eyes, his thin eyebrows furrowing.
+
+“Are you accusing me of stealing your phone?”
+
+Oh, god.
+
+“No, no, not at all, I just thought that…”
+
+He cuts you off, raising a hand.
+
+“No, no. I was joking. I have your phone here.”
+
+He rummages in his pocket, and as he does his name tags catches your eye again.
+
+“So you’re a doctor?”
+
+“Yes, I just work as a bouncer to pay for my collection of custom Monopoly sets and Tchaikovsky vinyls. I actually lecture Computer Science, you see.”
+
+You can’t help but raise your eyebrows in surprise. He catches the incredulous look on your face and frowns.
+
+The softly spoken Russian man advances towards you, stuffing your phone back into his jacket pocket.
+
+“Is there something funny about Monopoly?”
+
+In your drunken mind, there’s only one way this is going to end. A showdown with a softly spoken Russian bouncer with an affinity for Monopoly.
+
+You hear the Pokémon combat music play in your head and raise your fists.
+""")
+
+
     else :
         print("Talk to who?")
         input("------->PRESS ENTER TO CARRY ON<------")
@@ -316,9 +497,29 @@ def fight():
         print("THAT DID NOT WORK")
         print("")
     print("""
+“You…you win…” he manages to mutter, fumbling your phone out of his pocket.
+
+As you pick it up, the door of the bar bursts open as the loud, thunderous beat of the Russian National Anthem fills your ears.
+
+Your chest fills with the pride of a nation, you have defeated one of its greatest representatives in the Western world.
+
+The bartender, your friend, the bearded girl and everyone streams out to congratulate you, baptizing you in a stream of the purest Vodka you’ve ever tasted.
+
+As the holy liquid covers you and pools around your feet, the crowd suddenly begins to chant ‘One of us….One of us…..’
+
+You look down, catching a glimpse of your reflection in the glistening liquid. Gone were your mediocre English features, replaced by the features of a small,
+
+soft-spoken Russian face you’ve been seeing all night. You feel your knees weaken, your hands shaking as your lips part to mutter out the new name you’ve inherited.
+
+“My name is…..Kirril…..Sidorov….
+""")
+
+    input("------->PRESS ENTER TO GO HOME<------")
+
+    print("""
             //////////////////////////////////////////////////////////////////////////////////////////////////////////////
                         CONGRATS!
-        YOU HAVE DEFEATED THE BOUNCER, YOUR PHONE HAS BEEN RETURNED TO YOU
+        YOUR PHONE HAS BEEN RETURNED TO YOU
         YOU GRAB YOUR MATE AND GET IN TO THE CAB BUT JUST BEFORE YOU LEAVE A WOMAN SHOUTS OUT,
         ITS THE WOMAN FROM THE TABLE WHO YOU GOT THE NUMBER FROM.........
         "THANKS FOR DEFEATING THE BOUNCER HES WAS A TWAT" SHE SAYS TO YOU AND ASKED TO COME BACK TO YOURS
@@ -358,7 +559,7 @@ def print_map():
 
 current_area = booth_area
 inventory = []
-
+talk_too_girl = False
 
 
 
